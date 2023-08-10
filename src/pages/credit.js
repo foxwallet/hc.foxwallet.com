7c5b9d880997a2ef014884c4de2322fea519d3af
CodeFace <icodeface@users.noreply.github.com>
2023-08-10 14:49:26 +0800

    add credit page

---
+++ src/pages/credit.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function FoxCredit() {
+  return (
+    <Layout title="Fox Credit" description="Fox Credit">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          fontSize: '20px',
+        }}>
+      </div>
+      <script>
+        window.location.href="https://link.foxwallet.com/credit"
+      </script>
+    </Layout>
+  );
+}
